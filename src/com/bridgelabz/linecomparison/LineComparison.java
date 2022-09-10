99375a6ddc7f53136fa9b2492ffd7ce967ad39dc
sujit <sujitbokade2014@gmail.com>
2022-09-10 13:09:05 +0530

    [Sujit]Compare Two Lines

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -6,6 +6,8 @@ public class LineComparison {
     public static <length2> void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
         Scanner sc = new Scanner(System.in);
+
+        //Take Input
         System.out.println("--Enter First Line Points--");
         System.out.println("Enter a Point x1");
         int x1 = sc.nextInt();
@@ -16,10 +18,12 @@ public class LineComparison {
         System.out.println("Enter a Point y2");
         int y2 = sc.nextInt();
 
+        //Calculation & Display Part
         double length1 = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
         System.out.println("Length of First Line is " + length1);
         System.out.println();
 
+        //Take Input
         System.out.println("--Enter Second Line Points--");
         System.out.println("Enter a Point a1");
         int a1 = sc.nextInt();
@@ -30,9 +34,11 @@ public class LineComparison {
         System.out.println("Enter a Point b2");
         int b2 = sc.nextInt();
 
+        //Calculation & Display Part
         double length2 = Math.sqrt((Math.pow((a2 - a1), 2)) + (Math.pow((b2 - b1), 2)));
         System.out.println("Length of Second Line is " + length2);
 
+        //Compare Two Lines
         if (length1 == length2) {
             System.out.println("Both Lines are Equals");
         } else if (length1 > length2) {
